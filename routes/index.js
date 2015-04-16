ed6fed980f4322b0b60589eec74b654172cd2e57
Aaron Bean <aaron.bean@beardon.com>
2015-04-16 12:18:09 -0500

    very simple controls via web

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,38 @@ module.exports = function (app) {
     res.send(200);
   });
 
+  app.get('/turret/:command', function (req, res, next) {
+    var duration = req.query.duration || 250;
+    var shots = req.query.shots || 1;
+    var promise;
+    switch (req.param.command) {
+      case 'down':
+        promise = cheeky.moveDownAsync(duration);
+        break;
+      case 'left':
+        promise = cheeky.moveLeftAsync(duration);
+        break;
+      case 'right':
+        promise = cheeky.moveRightAsync(duration);
+        break;
+      case 'up':
+        promise = cheeky.moveUpAsync(duration);
+        break;
+      case 'stop':
+        promise = cheeky.stopAsync();
+        break;
+      case 'fire':
+        promise = cheeky.fireAsync(shots);
+        break;
+      case 'park':
+        promise = cheeky.parkAsync(duration);
+    }
+    return promise
+        .then(function () {
+          return res.send(200);
+        })
+  });
+
   app.get('/down', function (req, res, next) {
     return cheeky.moveDownAsync(200)
         .then(function () {
