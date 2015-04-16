4ba92be60d68dded8b2d98db4e3f4caa47f80f93
tyscorp <tyscorp@gmail.com>
2015-04-16 14:52:08 -0500

    add execute

--- routes/index.js
+++ routes/index.js
@@ -7,54 +7,55 @@ var cheeky = Promise.promisifyAll(require('dream-cheeky-driver'));
 
 module.exports = function (app) {
 
-  app.get('/', function (req, res, next) {
-    res.send(200);
-  });
+    app.get('/', function (req, res, next) {
+        res.send(200);
+    });
 
-  app.get('/turret/:command', function (req, res, next) {
-    var duration = req.query.duration || 250;
-    var shots = req.query.shots || 1;
-    var promise;
-    switch (req.param.command) {
-      case 'down':
-        promise = cheeky.moveDownAsync(duration);
-        break;
-      case 'left':
-        promise = cheeky.moveLeftAsync(duration);
-        break;
-      case 'right':
-        promise = cheeky.moveRightAsync(duration);
-        break;
-      case 'up':
-        promise = cheeky.moveUpAsync(duration);
-        break;
-      case 'stop':
-        promise = cheeky.stopAsync();
-        break;
-      case 'fire':
-        promise = cheeky.fireAsync(shots);
-        break;
-      case 'park':
-        promise = cheeky.parkAsync(duration);
-    }
-    return promise
-        .then(function () {
-          return res.send(200);
-        })
-  });
+    app.get('/turret/:command', function (req, res, next) {
+        var duration = req.query.duration || 250;
+        var shots = req.query.shots || 1;
 
-  app.get('/down', function (req, res, next) {
-    return cheeky.moveDownAsync(200)
-        .then(function () {
-          return res.send(200);
-        })
-  });
+        return Promise.try(function () {
+            switch (req.param.command.toLowerCase()) {
+                case 'down':
+                    return cheeky.moveDownAsync(duration);
+                case 'left':
+                    return cheeky.moveLeftAsync(duration);
+                case 'right':
+                    return cheeky.moveRightAsync(duration);
+                case 'up':
+                    return cheeky.moveUpAsync(duration);
+                case 'stop':
+                    return cheeky.stopAsync();
+                case 'fire':
+                    return cheeky.fireAsync(shots);
+                default:
+                    return cheeky.parkAsync(duration);
+            })
+            .then(function () {
+                return res.send(200);
+            });
+        });
+    });
 
-  app.get('/up', function (req, res, next) {
-    return cheeky.moveUpAsync(200)
-        .then(function () {
-          return res.send(200);
-        })
-  });
+    app,get('/turret/exec/:commands', function (req, res) {
+        return cheeky.executeAsync(req.param.commands)
+            .then(function () {
+                return res.send(200);
+            });
+    });
 
+    app.get('/down', function (req, res, next) {
+        return cheeky.moveDownAsync(200)
+            .then(function () {
+                return res.send(200);
+            });
+    });
+
+    app.get('/up', function (req, res, next) {
+        return cheeky.moveUpAsync(200)
+            .then(function () {
+                 return res.send(200);
+            });
+    });
 };
